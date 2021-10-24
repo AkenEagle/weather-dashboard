1f25f250e7c1135c6014d52cd11d7a2d5d373f96
AkenEagle <akeneagle@gmail.com>
2021-10-25 00:11:00 +0100

    search function test

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,14 @@
-$("#searchForm").submit((event) => {
-  console.log("search triggered");
+// When clicking on the search button
+$("#searchForm").submit(async (event) => {
+  // Get city name from input
+  const city = $("#searchInput").val();
+
+  // Search in the api
+  if (city.length > 0) {
+    console.log(city);
+  } else {
+    console.log("please enter a city");
+  }
+
   event.preventDefault();
 });
